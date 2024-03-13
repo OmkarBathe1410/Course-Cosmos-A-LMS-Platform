c9f1f14335d032f44f71b607eff3a5debb02ea5f
Omkar Satish Bathe <omi.bathe@gmail.com>
2024-03-13 17:12:40 +0530

    Updated [13-03-2024]

---
+++ server/app.ts
@@ -0,0 +1,7 @@
+// We first import the 'express' module, which is a popular Node.js framework for building web applications. 
+// The 'express' module simplifies the process of creating robust and scalable web servers.
+import express from "express";
+
+// Next, we create an instance of the Express application by calling the 'express()' function. 
+// This instance, represented by the constant 'app', will be used to define routes, middleware, and other functionalities required for our web application.
+export const app = express();
