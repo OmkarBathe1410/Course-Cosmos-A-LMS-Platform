d90f18cf8c43fec0d0dbd43cdefe7c34ebc23451
Omkar Satish Bathe <omi.bathe@gmail.com>
2024-05-19 02:37:45 +0530

    Updated [19-05-2024-02:37-Day38StripeIntegration]

--- client/redux/features/orders/ordersApi.ts
+++ client/redux/features/orders/ordersApi.ts
@@ -9,7 +9,40 @@ export const ordersApi = apiSlice.injectEndpoints({
         credentials: "include" as const,
       }),
     }),
+    getStripePublishableKey: builder.query({
+      query: () => ({
+        url: `payment/stripepublishablekey`,
+        method: "GET",
+        credentials: "include" as const,
+      }),
+    }),
+    createPaymentIntent: builder.mutation({
+      query: (amount) => ({
+        url: `payment`,
+        method: "POST",
+        body: {
+          amount,
+        },
+        credentials: "include" as const,
+      }),
+    }),
+    createOrder: builder.mutation({
+      query: ({ courseId, payment_info }) => ({
+        url: `create-order`,
+        method: "POST",
+        body: {
+          courseId,
+          payment_info,
+        },
+        credentials: "include" as const,
+      }),
+    }),
   }),
 });
 
-export const { useGetAllOrdersQuery } = ordersApi;
+export const {
+  useGetAllOrdersQuery,
+  useGetStripePublishableKeyQuery,
+  useCreatePaymentIntentMutation,
+  useCreateOrderMutation,
+} = ordersApi;
