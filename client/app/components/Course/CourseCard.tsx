33b386fe3a053ceff18333b5cf20d7c7ed1723b9
Omkar Satish Bathe <omi.bathe@gmail.com>
2024-05-19 20:49:51 +0530

    Updated [19-05-2024-20:49]

--- client/app/components/Course/CourseCard.tsx
+++ client/app/components/Course/CourseCard.tsx
@@ -12,7 +12,7 @@ type Props = {
 const CourseCard: FC<Props> = ({ item, isProfile }) => {
   return (
     <Link
-      href={!isProfile ? `/course/${item._id}` : `/courses-access/${item._id}`}
+      href={!isProfile ? `/course/${item._id}` : `/course-access/${item._id}`}
     >
       <div className="w-full !min-h-[35vh] dark:bg-slate-500 font-Poppins dark:bg-opacity-20 backdrop-blur border dark:border-[#ffffff1d] border-[#00000015] rounded-lg p-3 shadow-xl dark:shadow-[bg-slate-700] dark:shadow-inner">
         <Image
